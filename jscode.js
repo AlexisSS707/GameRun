6867a2db43e8ca68009f774d9bc871c98de3a907
Alexis S Silver <35941980+AlexisSS707@users.noreply.github.com>
2018-04-25 22:36:19 -0400

    Update jscode.js

--- jscode.js
+++ jscode.js
@@ -1,21 +1,9 @@
 /*Alexis S Silver
 Spring 2018
-Javascript code */
-	
-	//document.getElementById("insertName").innerHTML = "" + player + "";
-	//document.getElementById("insertGender").innerHTML = "" + gender + "";
-	function gameStart()	{            
-
-		//js their answers will be put into the script in <span>
-		alert("So " + player + " what do you want to do?");
-         window.location.href = "game.html";
-         var haha = new Audio('media/laugh.ogg');
-         haha.play();/*Alexis S Silver
-Spring 2018
 Javascript code 
 */
     /*click event is on ending.html.
-    mouseover event is on index.html*/
+    date manipulation, mouseover and mouseout event is on index.html*/
 		
    //playing Ether&Dream City - Tear Of A Shadow in index.html
 	var opening = new Audio('media/Tears.ogg');    
@@ -65,12 +53,14 @@ Javascript code
 		self.close();
 		}
 		
+		//start not needed stuff
 		var userName;
 		function inputName() {
          userName = document.getElementById('name').value;
          document.getElementById('nameField').innerHTML = userName;
 		}
-
+	//end not needed stuff
+	  
 	
 	//from game.html to index.html
 	function startover()   {
@@ -113,25 +103,28 @@ Javascript code
       } 
     }
 
+	
     var script =  ["start",
      "<br>It was storming. You still remembered that day clearly. You and your friend Haru ran into his house from the pounding rain. It was your first time at his house. You've never been here, yet you could not help how familiar it felt.",
       
-      "Haru<br>'Are you alright?'", 
+      "Haru<br>'That rain came out of nowhere, hunh?'", 
        "<br><b>'Yeah.'</b>",
-       "<br>Thunder rumbled and lightening flashed as his face turned dark.",
-       "<br>He asked,'Want to stay here Forever?'",
+       "<br>Thunder rumbled and lightening flashed as his changed to a dark expression.",
+       "<br>He locked the door.",
+       "Haru<br>'Want to stay here Forever?'",
         "choices",
-        "<br><b>Yes I will<b>",
-        "Haru<br> Wow...",
-        "Haru<br> I'm glad.",
-        "Haru<br> Now you can stay with me...",
-        "Haru<br><b><i>Forever.</i></b>",
+        "<br><b>'Yes I will.'<b>",
+        "Haru<br> 'Wow...'",
+        "Haru<br> 'I'm glad.'",
+        "Haru<br> 'Now you can stay with me...'",
+        "Haru<br><b><i>'Forever.'</i></b>",
         "<br>The clouds cleared as the sun shone through the opening.",
         "Haru<br> 'How about we go for a walk?'",
+        "<br>The eerie mood was suddenly broken from that one statement.",
         "<br><b>'What did you mean about Forever anyways?'</b>",
         "<br>Haru shrugged.",
         "Haru<br> 'I guess to be edgy.'",
-        "<br>You scoff at his response",
+        "<br>You scoff at his response. The weird mood was in your head after all.",
         "<br><b>'I'm going home.'</b>",
         "<br>Well, at least you didn't die.",
         "<br><i>Good End</i>"
@@ -158,80 +151,3 @@ Javascript code
 				}
 				counter++;
 			}
-		}
-// it closes the window
-	function exitGame()	{
-		alert("Please don't leave me.");
-		myWindow.close();
-		}
-
-	function startover()   {
-        window.location.href = "titlescreen.html";
-        }
-		
-		//all the models for my characters and parts
-		//helps it preload when using big images
-		var pics = ["bedone.jpg","happyend.jpg", "raindrops.jpg", "rainglass.png", "rainphoto.jpg", "girl.png","boy.png", "yanHaru.png"];
-		
-		//loop and length to make a images go in front of each word "saving time"
-		var picstuff = new Array(pics.length);
-		for (var i = 0; i < pics.length; i++)
-		    {
-		        picstuff[i] = new Image();
-		        picstuff[i].src = "media/" + pics[i];
-		    } 
-		//var yanHaru = document.getElementById("guy").src;
-		//var n;
-		//var gender = document.getElementById("character").src;
-		//var photo;
-		//var textBlock;
-		//placement
-		//var left;
-		//var right;
-		
-	/*function prepareNovel() {}
-        novel.imagePath = "media/"; // path to image directory
-        yanHaru = new Character("Haru", {color: "rgb(255,228,225)", postion: new Position(0, .75, 0, 1)});
-        you = new Character("you", {color: "rgb(240,248,255)", position: new Position(800, 450, 1, 1)});
-        n = new Character("Narrator",{color:"rgb(255,250,250)"});
-         
-        textArea = new TextBlock("myText");*/
-
-    var script =  [
-     "It was storming. I still remembered that day clearly. Haru and I ran into his house from the pounding rain. It was my first time at his house. I’ve never been here, yet I couldn’t help how familiar it felt.",
-      
-      "Haru: Are you alright?", 
-       "Yeah",
-        choices,
-        "Haru: What?",
-        "Haru: No,you have no say in the matter.",
-        "Haru: You played this game.",
-        "Haru: Do nOT PlaY WiTh My heART",
-        "Haru: Wow...",
-        "Haru: I'm glad.",
-        "Haru: Now you can stay with me...",
-        "Forever."
-        ];
-        //document.getElementById("dboxDiv").innerHTML = script[0];
-        document.getElementById("dboxDiv").innerHTML = script[0];
-        function moveFW()   {
-           var i = 0;
-           script.shift();
-           if (i < script.length){                
-                document.getElementById("dboxDiv").innerHTML = script[0];
-           }
-       }  
-    
-   var choices = [function choice() { 
-    /*   if (script[5] == script[5])  {
-    var yes = confirm("Will you stay?");
-    if (yes === true) {
-        document.getElementById("dboxDiv").innerHTML = script[9];
-    } else {
-        document.getElementById("dboxDiv").innerHTML = script[5];
-    }
-    }else{
-       document.getElementById("dboxDiv").innerHTML = script[i];
-    }*/
-     }]
-   
